cc29a40a1fbecf700293b8881e89a32e4eaa1f00
SarinSwift <sarinyaswift@gmail.com>
2018-11-06 11:12:46 -0800

    Added Monogdb and Post model

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/contractor-project');
 
 var exphbs = require('express-handlebars');
 
-let posts = [
-    { title: "Homemade organic cookies", foodTitle: "Good for all ages!" },
-    { title: "Organic acai powder", foodTitle: "Easily blend formula" }
-]
+// let posts = [
+//     { title: "Homemade organic cookies", foodTitle: "Good for all ages!" },
+//     { title: "Organic acai powder", foodTitle: "Easily blend formula" }
+// ]
+
+const Post = mongoose.model('Post', {
+    title: String,
+    foodTitle: String
+})
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 // INDEX
 app.get('/', (req, res) => {
-    res.render('posts-index', { posts: posts });
+    Post.find()
+        .then(posts => {
+            res.render('posts-index', { posts: posts });
+        })
+        .catch(err => {
+            console.log(err);
+        })
 })
 
 
