784554fc95f72d35527a25e55dd45a45691dbd2c
SarinSwift <sarinyaswift@gmail.com>
2018-11-06 10:47:01 -0800

    Users can see all posts

--- app.js
+++ app.js
@@ -3,12 +3,21 @@ const app = express()
 
 var exphbs = require('express-handlebars');
 
+let posts = [
+    { title: "Homemade organic cookies", foodTitle: "Good for all ages!" },
+    { title: "Organic acai powder", foodTitle: "Easily blend formula" }
+]
+
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+// INDEX
 app.get('/', (req, res) => {
-    res.render('home', { msg: 'Handlebars are cool!' });
+    res.render('posts-index', { posts: posts });
 })
+
+
+
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!')
